73651a5a9b96d4390f76ced035981ebef3bb30aa
EDZ <111>
2020-07-28 15:37:39 +0800

    配置项目

---
+++ web/src/main/java/club/tacbin/table/web/WebApplication.java
@@ -0,0 +1,16 @@
+package club.tacbin.table.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author EDZ
+ * @description
+ * @date 2020/7/21 18:28
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
